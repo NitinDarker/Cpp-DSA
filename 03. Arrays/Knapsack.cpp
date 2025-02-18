e1d42194ea6387db5af543cd45570d483915af7f
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-02-18 19:46:10 +0530

    Fractional Knapsack

---
+++ 03. Arrays/Knapsack.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+struct Item {
+    int value;
+    int weight;
+};
+
+class Solution {
+  public:
+    // Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(vector<int> &val, vector<int> &wt, int capacity) {
+        // 
+    }
+};
