98336d6e707b6ab82b231241dc4fef43e525e620
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-03-19 18:46:28 +0530

    Doing some bit manipulation

--- 18. Bit Manipulation/countBits.cpp
+++ 18. Bit Manipulation/countBits.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int countBits(int n) {
+// int countBits(int n) {
+//     int count = 0;
+//     while (n > 0) {
+//         int lastDig = n & 1;
+//         count += lastDig;
+//         n = n >> 1;
+//     }
+//     return count;
+// }
+
+// Faster Way
+int countBits(int num) {
     int count = 0;
-    while (n > 0) {
-        int lastDig = n & 1;
-        count += lastDig;
-        n = n >> 1;
+    while (num > 0) {
+        num = num & (num - 1); // Clears the last set bit
+        count++;
     }
     return count;
 }
