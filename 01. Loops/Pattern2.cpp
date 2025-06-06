a2cef6c5a8f14389a6ba267a2c3722609b8af954
NitinDarker <nitinsharma9829547681@gmail.com>
2025-06-06 08:44:15 +0530

    ListNode

--- 01. Loops/Pattern2.cpp
+++ 01. Loops/Pattern2.cpp
@@ -2,21 +2,19 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-     int n = 4;
-     for(int i=0; i<n; i++){
-          cout << "* ";
-          for(int j=0; j<n-1; j++){
-               if(i==0 || i==n-1){
-                    cout << "* ";
-               }
-               else{
-                    cout << "  ";
-               }
-          }
-          cout << '*' << endl;
-     }
+int main() {
+    int n = 4;
+    for (int i = 0; i < n; i++) {
+        cout << "* ";
+        for (int j = 0; j < n - 1; j++) {
+            if (i == 0 || i == n - 1) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << '*' << endl;
+    }
 
-     return 0;
+    return 0;
 }
