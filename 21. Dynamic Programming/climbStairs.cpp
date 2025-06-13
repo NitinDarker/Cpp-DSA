d1fc6657ffe5a9d155a957cc7b7242ad46fd50dc
NitinDarker <nitinsharma9829547681@gmail.com>
2025-06-13 23:15:47 +0530

    dp started again

--- 21. Dynamic Programming/climbStairs.cpp
+++ 21. Dynamic Programming/climbStairs.cpp
@@ -1,11 +1,10 @@
+// https://leetcode.com/problems/climbing-stairs/description/
 #include <bits/stdc++.h>
 using namespace std;
 
 int dpClimb(int n, vector<int> &dp) {
-    if (n <= 1)
-        return 1;
-    if (dp[n] != -1)
-        return dp[n];
+    if (n <= 1) return 1;
+    if (dp[n] != -1) return dp[n];
     dp[n] = dpClimb(n - 1, dp) + dpClimb(n - 2, dp);
     return dp[n];
 }
