4a1e204a0f141ad087dd21d2b5b7cf04fd66541c
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-08-04 13:44:13 +0530

    kadane algorithm

--- 03. Arrays/Kadane.cpp
+++ 03. Arrays/Kadane.cpp
@@ -1,28 +1,26 @@
 // Sub-array is a continuous part of an array
-#include <iostream>
 #include <climits> // Header file to include INT_MIN macro
+#include <iostream>
 using namespace std;
 
 // This is the best way to solve (Using DP)
-int sub_array_sum(int *arr, int n)
-{ // Max Sub-array sum = Sum of all elements of array (Only if all the elements are positive)
-     int max_sum = INT_MIN;
-     int sum = 0;
-     for (int i = 0; i < n; i++){
-          sum += arr[i];
-     
-          max_sum = max(sum, max_sum);
+int sub_array_sum(int *arr, int n) { // Max Sub-array sum = Sum of all elements of array (Only if all the elements are positive)
+    int max_sum = INT_MIN;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
 
-          if(sum < 0)
-               sum = 0;
-     }
-     return max_sum;
+        max_sum = max(sum, max_sum);
+
+        if (sum < 0)
+            sum = 0;
+    }
+    return max_sum;
 } // Time Complexity = O(n) -> Best
 
-int main()
-{
-     int arr[] = {-2, -3, -6, -5, -4, -8};
-     int size = sizeof(arr) / sizeof(int);
-     cout << "Max Subarray sum: " << sub_array_sum(arr, size);
-     return 0;
+int main() {
+    int arr[] = {-2, -3, -6, -5, -4, -8};
+    int size = sizeof(arr) / sizeof(int);
+    cout << "Max Subarray sum: " << sub_array_sum(arr, size);
+    return 0;
 }
