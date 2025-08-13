ef3f70a3e79bbcc66567942c87467ee3b68f5b16
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-08-13 15:29:48 +0530

    Topological Sort + Kahn's Algorithms

--- 17. Graph/Topological Sort/topoSort.cpp
+++ 17. Graph/Topological Sort/topoSort.cpp
@@ -10,14 +10,38 @@ vector<vector<int>> createAdj(const vector<vector<int>> &edges, const int V) {
     return adj;
 }
 
-// DFS
+// DFS (Topological Sort Algorithm)
 void dfs(int start, const vector<vector<int>> &adj, vector<bool> &visited, stack<int> &st) {
     visited[start] = true;
     for (int node : adj[start]) {
-        if (!visited[node])
-            dfs(node, adj, visited, st);
+        if (!visited[node]) dfs(node, adj, visited, st);
     }
     st.push(start);
+}
+
+// BFS (Kahn's Algorithm)
+vector<int> bfs(vector<vector<int>> &adj) {
+    int V = adj.size();
+    vector<int> inDegree(V, 0);
+    for (int i = 0; i < V; i++) {
+        for (int nei : adj[i]) {
+            inDegree[nei]++;
+        }
+    }
+    queue<int> q;
+    vector<int> topo;
+    for (int i = 0; i < V; i++) {
+        if (inDegree[i] == 0) q.push(i);
+    }
+    while (!q.empty()) {
+        int node = q.front(); q.pop();
+        topo.push_back(node);
+        for (int nei : adj[node]) {
+            inDegree[nei]--;
+            if (inDegree[nei] == 0) q.push(nei);
+        }
+    }
+    return topo;
 }
 
 vector<int> topoSort(int V, vector<vector<int>> &edges) {
@@ -34,5 +58,6 @@ vector<int> topoSort(int V, vector<vector<int>> &edges) {
         sorted.push_back(st.top());
         st.pop();
     }
-    return sorted;
+    // return sorted;
+    return bfs(adj);
 }
